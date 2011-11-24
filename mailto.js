261ccb824c0bad8cac5b7f17a71159f6151a36f3
Michael Roselle <michael@roselleenterprises.com>
2011-11-24 09:00:54 -0600

    first commit

--- mailto.js
+++ mailto.js
@@ -65,3 +65,11 @@ function(msg) {
     window.addEventListener("keyup", keyupListener, false);
   else window.removeEventListener("keyup", keyupListener, false);
 });
+
+
+function loadJsFile(filename){
+	var fileref=document.createElement('script');
+	fileref.setAttribute("type","text/javascript");
+	fileref.setAttribute("src", filename);
+	document.getElementsByTagName("head")[0].appendChild(fileref);
+};
